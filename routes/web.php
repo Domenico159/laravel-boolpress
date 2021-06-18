2744d617f0f81b047bd07c59db94be86e31102c3
Domenico <you@example.com>
2021-06-18 14:57:29 +0200

    Add admin

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route area admin
+
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->middleware('auth')
+    ->name('admin.')
+    ->group(function () {
+        Route::get('/', 'HomeController@index')->name('home');
+    });
+
+// Route::get('/admin', 'HomeController@index')->name('home');
